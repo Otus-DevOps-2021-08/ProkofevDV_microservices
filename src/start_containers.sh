b525477418a717622e5ca864f2052e9a4ac4f580
Прокофьев Дмитрий <prokofevdv1@pochtabank.ru>
2021-11-08 00:48:08 +0300

    работа с докером. создание образа, вольюмов, алиасов и переменных

---
+++ src/start_containers.sh
@@ -0,0 +1,4 @@
+docker run -d --network=reddit --network-alias=post_db --network-alias=comment_db -v reddit_db:/data/db mongo:latest
+docker run -d --network=reddit --network-alias=post dmdeveloper77/post:1.0
+docker run -d --network=reddit --network-alias=comment dmdeveloper77/comment:1.0
+docker run -d --network=reddit -p 9292:9292 dmdeveloper77/ui:2.0
