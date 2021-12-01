d8c8eec513d779b00317babb8ffa158f88f9ba41
Прокофьев Дмитрий <prokofevdv1@pochtabank.ru>
2021-12-02 01:53:58 +0300

    работа с прометеус. создание сервиса в докере, проверка рабтоы мониторинга и хелсчеков

--- docker/start_containers.sh
+++ docker/start_containers.sh
@@ -1,4 +1,5 @@
 docker run -d --network=reddit --network-alias=post_db --network-alias=comment_db -v reddit_db:/data/db mongo:latest
 docker run -d --network=reddit --network-alias=post dmdeveloper77/post:1.0
 docker run -d --network=reddit --network-alias=comment dmdeveloper77/comment:1.0
-docker run -d --network=reddit -p 9292:9292 dmdeveloper77/ui:2.0
+docker run -d --network=reddit -p 9292:9292 dmdeveloper77/ui:1.0
+docker run -d --network=reddit -p 9090:9090 dmdeveloper77/prometheus
